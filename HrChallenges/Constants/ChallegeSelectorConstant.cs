c67605df61f9f0856423487e6ccd261befe0e890
Hugo Velazquez <hugo.velazquez.g@outlook.com>
2023-04-13 10:05:29 -0600

    feat(4): Adding Time TimeConversion challenge

--- HrChallenges/Constants/ChallegeSelectorConstant.cs
+++ HrChallenges/Constants/ChallegeSelectorConstant.cs
@@ -5,6 +5,7 @@ internal static class ChallegeSelectorConstant
     public const string HeaderMessage = "Write the Challenge option you want to excecute and the press enter: ";
     public const string HeaderPlusMinus = "Enter an array of n Numbers separated with a space and press enter: ";
     public const string HeaderMinMax = "Enter an array of 5 Numbers separated with a space and press enter: ";
+    public const string HeaderTimeConversion = "Enter a string with the next format hh:mm:sstt enter: ";
 
     public const string ValidationError = "The value entered is not correct. Try again.";
 }
