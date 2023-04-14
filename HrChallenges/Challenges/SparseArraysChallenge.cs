dbd9487d1672fa2cdacd79c996ea752a07718f8a
Hugo Velazquez <hugo.velazquez.g@outlook.com>
2023-04-14 15:25:56 -0600

    feat(8): Solving Sparse Arrays

---
+++ HrChallenges/Challenges/SparseArraysChallenge.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace HrChallenges.cmd.Challenges;
+
+internal class SparseArraysChallenge : IChallenge
+{
+    public void StartChallengeConsole()
+    {
+        int stringsCount = Convert.ToInt32(Console.ReadLine()!.Trim());
+        List<string> strings = ValueReader.GetListNStrings(stringsCount);
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine()!.Trim());
+        List<string> queries = ValueReader.GetListNStrings(queriesCount);
+
+        List<int> res = MatchingStrings(strings, queries);
+
+        foreach (var item in res)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static List<int> MatchingStrings(List<string> strings, List<string> queries)
+    {
+        int result = 0;
+        List<int> results = new();
+
+        foreach (string query in queries)
+        {
+            foreach (string value in strings)
+            {
+                if (value == query)
+                    result++;
+            }
+            results.Add(result);
+            result = 0;
+        }
+
+        return results;
+    }
+
+    public void Validation()
+    {
+        throw new NotImplementedException();
+    }
+}
