49740130c846e618aad5685c0e69bb4ac5083521
Hugo Velazquez <hugo.velazquez.g@outlook.com>
2025-08-28 18:30:21 -0600

    feat(18): BigO Notation and Array. Study Plan

---
+++ HrChallenges/Challenges/LinearSearchChallenge.cs
@@ -0,0 +1,31 @@
+﻿namespace HrChallenges.cmd.Challenges;
+
+internal class LinearSearchChallenge : IChallenge
+{
+    public void StartChallengeConsole()
+    {
+        Console.WriteLine(ChallengeSelectorConstant.HeaderInsertArrayNNumbers);
+        List<int> ints = ValueReader.GetIntValuesFromString();
+
+        Console.WriteLine(ChallengeSelectorConstant.HeaderValueToSearch);
+        int.TryParse(Console.ReadLine(), out int number);
+
+        Console.WriteLine(LinearSeach(ints, number));
+    }
+
+    private int LinearSeach(List<int> numbers, int number)
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == number)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public void Validation()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HrChallenges/Challenges/ReversStringInPlaceChallenge.cs
@@ -0,0 +1,34 @@
+﻿namespace HrChallenges.cmd.Challenges
+{
+    internal class ReversStringInPlaceChallenge : IChallenge
+    {
+        public void StartChallengeConsole()
+        {
+            Console.WriteLine(ChallengeSelectorConstant.HeaderAlphanumericNoSpaces);
+            string input = Console.ReadLine()!;
+
+            Console.WriteLine(ReverseStringInPlace(input.ToCharArray()));
+        }
+
+        private string ReverseStringInPlace(char[] input)
+        {
+            int max = input.Length - 1;
+            char caracter = new();
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                caracter = input[max];
+                input[max] = input[i];
+                input[i] = caracter;
+
+                max--;
+            }
+
+            return new string(input);
+        }
+
+        public void Validation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
