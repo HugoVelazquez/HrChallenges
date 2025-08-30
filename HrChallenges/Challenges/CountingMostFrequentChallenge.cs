409de651b2fd24967e30ae9e38f57ac7aeb017a0
Hugo Felix Velazquez Gonzalez <hugo.velazquez.g@outlook.com>
2025-08-29 18:08:50 -0600

    feat(19): O(n) O(1) Hash Maps. Study Plan (#16)

---
+++ HrChallenges/Challenges/CountingMostFrequentChallenge.cs
@@ -0,0 +1,44 @@
+﻿namespace HrChallenges.cmd.Challenges;
+
+internal class CountingMostFrequentChallenge : IChallenge
+{
+    public void StartChallengeConsole()
+    {
+        Console.WriteLine(ChallengeSelectorConstant.HeaderInsertArrayNNumbers);
+        List<int> ints = ValueReader.GetIntValuesFromString();
+
+        Console.WriteLine(CountingMostFrequest(ints));
+    }
+
+    private int CountingMostFrequest(List<int> numbers)
+    {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+
+        int n = numbers.Count;
+
+        for (int i = 0; i < n; i++)
+            freq[numbers[i]] = freq.ContainsKey(numbers[i]) ? freq[numbers[i]] + 1 : 1;
+
+        int max = 0, result = -1;
+
+        foreach (var entry in freq)
+        {
+            int value = entry.Key, count = entry.Value;
+
+            if (count > max || count == max && value > result)
+            {
+                max = count;
+                result = value;
+            }
+        }
+
+        return result;
+
+
+    }
+
+    public void Validation()
+    {
+        throw new NotImplementedException();
+    }
+}
