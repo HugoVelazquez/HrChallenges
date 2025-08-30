9e2391b8d428c5f079ae74ab4f11748b264c52d8
Hugo Felix Velazquez Gonzalez <hugo.velazquez.g@outlook.com>
2025-08-30 11:11:49 -0600

    feat(20): O(n) O(1) Reverse List (#17)

---
+++ HrChallenges/Common/SinglyNode.cs
@@ -0,0 +1,22 @@
+﻿namespace HrChallenges.cmd.Common
+{
+    internal class SinglyNode
+    {
+        public int Data;
+        public SinglyNode? Next;
+
+        public SinglyNode(int data)
+        {
+            this.Data = data;
+        }
+
+        public SinglyNode()
+        {
+            this.Data = 1;
+            this.Next = new SinglyNode(2);
+            this.Next.Next = new SinglyNode(3);
+            this.Next.Next.Next = new SinglyNode(4);
+            this.Next.Next.Next.Next = new SinglyNode(5);
+        }
+    }
+}
