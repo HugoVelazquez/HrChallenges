984e2216ddd039c11fb96ec75f7e791a0e8d9ec3
Hugo Velazquez <hugo.velazquez.g@outlook.com>
2025-10-13 21:09:51 -0600

    feat(24): RemoveAllOccurrences

---
+++ HrChallenges/Challenges/TwoPointersProblems/RemoveAllOccurrencesChallenge.cs
@@ -0,0 +1,37 @@
+﻿namespace HrChallenges.cmd.Challenges.TwoPointersProblems;
+
+public class RemoveAllOccurrencesChallenge : IChallenge
+{
+    public void StartChallengeConsole()
+    {
+        // https://www.geeksforgeeks.org/dsa/remove-element/
+
+        Console.WriteLine(ChallengeSelectorConstant.HeaderInsertArrayNNumbers);
+        List<int> ints = ValueReader.GetIntValuesFromString();
+
+        Console.WriteLine(ChallengeSelectorConstant.HeaderValueToSearch);
+
+        int.TryParse(Console.ReadLine(), out int ele);
+
+        Console.WriteLine(RemoveAllOccurrences(ints, ele));
+    }
+
+    private int RemoveAllOccurrences(List<int> ints, int ele)
+    {
+        int k = 0;
+        for (int i = 0; i < ints.Count; i++)
+        {
+            if (ints[i] != ele)
+            {
+                ints[k] = ints[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public void Validation()
+    {
+        throw new NotImplementedException();
+    }
+}
